8cb1749b38d0c6e8d8b9631b08fd4571dc66fb9f
daniel <daceval@fi.uba.ar>
2020-07-24 20:52:12 -0300

    abb por rangos y tda modificado

--- abb/abb.c
+++ abb/abb.c
@@ -9,23 +9,27 @@
 
 /******ESTRUCTURAS*******/
 
-typedef struct nodo_abb {
+typedef struct nodo_abb 
+{
 	struct nodo_abb* izq;
 	struct nodo_abb* der;
 	char* clave;
 	void* dato;
+
 }nodo_abb_t;
 
 
 
-struct abb {
+struct abb 
+{
 	nodo_abb_t* raiz;
 	size_t cant;
 	abb_destruir_dato_t destruir;
 	abb_comparar_clave_t comparar;
 };
 
-struct abb_iter{
+struct abb_iter
+{
 	pila_t* pila;
 };
 
